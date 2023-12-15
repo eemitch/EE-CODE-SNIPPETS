27948102f75fef531dfc3c1a1bacf6563520de8d
Mitchell Bennis <48490506+eemitch@users.noreply.github.com>
2023-12-15 16:47:07 -0600

    Initial Commit

---
+++ ee-input-sanitization.php
@@ -0,0 +1,57 @@
+<?php // Mitchell Bennis (mitch@elementengage.com)
+// Version 1.1 -- Rev 12.15.23
+// PHP 8.3 Approved
+	
+	// Sanitize Form and $_REQUEST Inputs
+	// Returns FALSE if sanitization occurred.
+	function eeSanitizeInput($eeInput, $eeType = 'text') { // text, email, url, num, float, ip
+		
+		if(empty($eeInput)) { return FALSE; }
+		
+		$eeOutput = $eeInput; // Any changes will return FALSE
+		
+		switch ($eeType) {
+			case 'email':
+				// Sanitize email
+				$eeInput = strtolower(filter_var($eeInput, FILTER_SANITIZE_EMAIL));
+				break;
+			case 'url':
+				// Sanitize URL
+				$eeProtocol = preg_match('/^https?:\/\//', $eeInput); // Check if protocol is already included
+				$eeURL = $eeProtocol ? $eeInput : 'http://' . $eeInput; // Prepend 'http://' for validation if no protocol is present
+				if (filter_var($eeURL, FILTER_VALIDATE_URL) !== FALSE) { // Validate the URL
+					$eeInput = preg_replace('/^https?:\/\//', '', $eeURL);
+				} else {
+					$eeInput = FALSE;
+				}
+				break;
+			case 'num':
+				// Sanitize number
+				$eeInput = filter_var($eeInput, FILTER_SANITIZE_NUMBER_INT);
+				break;
+			case 'float':
+				// Sanitize float number
+				$eeInput = filter_var($eeInput, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+				$eeInput = floatval($eeInput);
+				break;
+			case 'ip':
+				// Validate IP address
+				$eeInput = filter_var($eeInput, FILTER_VALIDATE_IP);
+				if(empty($eeInput)) { $eeInput = FALSE; }
+				break;
+			default:
+				// Sanitize text
+				if(strlen($eeInput)) {
+					$eeInput = htmlspecialchars($eeInput, ENT_QUOTES, 'UTF-8');
+				}
+				break;
+		}
+		
+		if($eeInput == $eeOutput) {
+			return trim($eeOutput);
+		}
+		
+		return FALSE;
+	}
+?>
+						

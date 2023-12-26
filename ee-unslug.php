4ed5c45e19b218ae183c6c83595722196ec34a41
Mitchell Bennis <48490506+eemitch@users.noreply.github.com>
2023-12-26 11:32:49 -0600

    Update ee-unslug.php

--- ee-unslug.php
+++ ee-unslug.php
@@ -1,6 +1,17 @@
 <?php // Mitchell Bennis (mitch@elementengage.com)
-// Version 1.1 -- Rev 12.15.23
+// Version 1.2 -- Rev 12.26.23
 // PHP 8.3 Approved
+
+
+// Create a slug
+// Example: My Super Thing --> my-super-thing
+function eeMakeSlug($eeString){
+   $eeSlug = preg_replace('/[^A-Za-z0-9-]+/', '-', $eeString);
+   $eeSlug = strtolower($eeSlug);
+   return $eeSlug;
+}
+
+echo eeMakeSlug('My Super Thing');
 
 // Unslug a slug
 // Example: my-super_thing --> My Super Thing
